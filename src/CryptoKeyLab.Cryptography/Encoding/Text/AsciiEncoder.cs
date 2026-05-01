cff4f20770566be6dfbc4d6ae08b7f060b9935b7
THAxSENSEI <THAxSENSEI@THAxSENSEI.COM>
2026-05-01 20:54:44 +0530

    intial commit addeding encoding feature in software.

---
+++ src/CryptoKeyLab.Cryptography/Encoding/Text/AsciiEncoder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoKeyLab.Cryptography.Encoding.Text
+{
+    internal class AsciiEncoder
+    {
+    }
+}
